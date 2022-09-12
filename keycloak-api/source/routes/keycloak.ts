dca01c08d2a0476850d873ba86860a8d39a19993
Bruno_Garcia1 <bruno_garcia1@dellteam.com>
2022-09-12 20:35:26 -0300

    WIP conecting endpoints to api controller

--- keycloak-api/source/routes/keycloak.ts
+++ keycloak-api/source/routes/keycloak.ts
@@ -2,10 +2,19 @@ import express from 'express';
 import controller from '../controllers/keycloak';
 const router = express.Router();
 
-router.get('/posts', controller.getPosts);
-router.get('/posts/:id', controller.getPost);
-router.put('/posts/:id', controller.updatePost);
-router.delete('/posts/:id', controller.deletePost);
-router.post('/posts', controller.addPost);
+router.get('/getUser', controller.getUser);
+router.post('/postUser', controller.addUser);
 
 export = router;
+
+// import express from 'express';
+// import controller from '../controllers/keycloak';
+// const router = express.Router();
+
+// router.get('/posts', controller.getPosts);
+// router.get('/posts/:id', controller.getPost);
+// router.put('/posts/:id', controller.updatePost);
+// router.delete('/posts/:id', controller.deletePost);
+// router.post('/posts', controller.addPost);
+
+// export = router;
