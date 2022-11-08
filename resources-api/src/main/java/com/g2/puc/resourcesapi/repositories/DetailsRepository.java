63a822d907dc521028adefecd4866e8a54e9a38c
Bruna <bruna.chiochetta@gmail.com>
2022-11-07 22:28:01 -0300

    Remove unused code

--- resources-api/src/main/java/com/g2/puc/resourcesapi/repositories/DetailsRepository.java
+++ resources-api/src/main/java/com/g2/puc/resourcesapi/repositories/DetailsRepository.java
@@ -1,7 +1,6 @@
 package com.g2.puc.resourcesapi.repositories;
 
 import com.g2.puc.resourcesapi.models.Detail;
-import com.g2.puc.resourcesapi.models.Resource;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
